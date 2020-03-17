a0a6c989d125a095b39baaf80318fd594849d5e5
RuanSena <ruansenadev@gmail.com>
2020-03-17 17:57:24 -0300

    layout improvements

--- controllers/resultadosController.js
+++ controllers/resultadosController.js
@@ -1,11 +1,23 @@
 const loteria = require('./data/loteria');
 // const fs = require ('fs');
 
+function orderPoste(extracoes) {
+    let poste = [];
+    extracoes.forEach((extracao, i) => { 
+        if(extracao.poste) {
+            poste.push(extracoes.splice(i, 1)[0]);
+        }
+    })
+    return poste.concat(extracoes);
+}
+
 exports.ultimas_extracoes = async function(req, res, next) {
     let limit = 20;
     let skip = 0;
     let extracoes = await loteria.req_ultimas();
+    extracoes = orderPoste(extracoes);
     extracoes = extracoes.slice(skip, limit);
+    //- order deu no poste
     if(!extracoes) {
         return next();
     }
