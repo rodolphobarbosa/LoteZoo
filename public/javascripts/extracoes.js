0ce781be05b915bfe7482e47fed6a6752109c9d6
RuanSena <ruansenadev@gmail.com>
2020-03-21 09:57:28 -0300

    modal result

--- public/javascripts/extracoes.js
+++ public/javascripts/extracoes.js
@@ -1,17 +1,58 @@
-$('#ResultadoModal').on('show.bs.modal', function(event) {
-	// botao da extracao target
-	let button = $(event.relatedTarget)
-	// Extrair informações do atributo data-*
-	let uri = button.data('uri')
-  let modal = $(this)
-	$.ajax({
-    url: uri,
-  }).done(sorteio => {
-    console.log(modal,sorteio);
-    modal.find('.modal-title').text(sorteio.banca + '-' + sorteio.extracao)
-    modal.find('.modal-body').text(sorteio.resultado)
-    modal.find('.sorteio-data').text(sorteio.data)
-  })
-	// If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-	// Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+function atualizaModal(modal, sorteio) {
+	modal.find('.sorteio-banca').text(sorteio.banca)
+	modal.find('.sorteio-extracao').text(sorteio.extracao)
+  modal.find('.sorteio-data').text(sorteio.data)
+  let resultado = $('<ul class="list-group list-group-flush sorteio-resultado"></ul>');
+  sorteio.resultado.forEach((premio, i) => {
+    resultado.append(`
+      <li class='list-group-item sorteio-premio ${i%2==0?"bg-striped":""}'>
+        <div class='row no-gutters row-cols-md-3 row-cols-lg-4'>
+          <div class='col pl-3 d-flex align-items-center justify-content-between flex-grow-1'>
+            <span class='border text-muted premio-num'>
+              ${i+1}º
+            </span>
+            <p class='premio-milhar ml-auto'>${premio.milhar}</p>
+          </div>
+          <div class='col d-flex align-items-center justify-content-end flex-shrink-1'>
+            <span class='grupo-num'>${premio.grupo.num}</span>
+          </div>
+          <div class='col d-flex align-items-center justify-content-center'>
+            <img class='grupo-img' src='/images/grupos/${premio.grupo.img}' alt='${premio.grupo.nome}'>
+          </div>
+          <div class='col pr-3 d-none d-lg-flex align-items-center justify-content-center'>
+            <span class='grupo-nome'>${premio.grupo.nome}</span>
+          </div>
+        </div>
+      </li>
+    `);
+  });
+  modal.find('.modal-body').html(resultado);
+}
+
+$(function() {
+	const sorteiosBtns = $('.sorteio-res')
+	const sorteioModal = $('#Modal')
+	sorteiosBtns.click(function(ev) {
+		const btn = $(this)
+		const extracao = btn.parentsUntil('.col', '.sorteio')
+		let sorteio_banca = extracao.find('.sorteio-banca').text()
+		let sorteio_extracao = extracao.find('.sorteio-extracao').text()
+		let sorteio_data = extracao.find('.sorteio-data').text()
+		console.log(sorteio_banca, sorteio_extracao, sorteio_data)
+		let uri = btn.data('uri')
+		$.ajax({
+			url: uri,
+			dataType: 'json',
+			error: function(jqXHR, status, erro) {
+				alert('status: ' + erro)
+			},
+			success: function(sorteio) {
+				// retornou sorteio atualiza o modal
+        atualizaModal(sorteioModal, sorteio);
+				sorteioModal.modal('show')
+				// sorteioModal.on('show.bs.modal', function(e) {
+				// })
+			}
+		})
+	})
 })
